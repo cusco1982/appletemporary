07c989ef6d51397674ae9ad6a980570521b2556f
Julian <cusco2891@gmail.com>
2021-01-18 12:44:26 -0500

    template stuff

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+
+    </div>
+  );
+}
+
+export default App;
