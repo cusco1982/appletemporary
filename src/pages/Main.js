1a779dac4ea69e8aec8b36c8d6f25d059bb99a1d
Julian <cusco2891@gmail.com>
2021-02-10 02:16:12 -0500

    services

--- src/pages/Main.js
+++ src/pages/Main.js
@@ -10,7 +10,14 @@ import img2 from "./photos/pexels2.jpeg";
 import img3 from "./photos/cranes.jpeg";
 import img1 from "./photos/pexels.jpeg";
 import phone from "./photos/phone.png";
+
 import insulationmask from "./photos/insulationmask.png"
+import safety from "./photos/safety.png"
+import truck from "./photos/truck.png"
+import insulation from "./photos/insulation.png"
+import consulting from "./photos/consulting.png"
+
+
 
 
 
@@ -134,10 +141,7 @@ class Main extends Component {
 
         <div style={{ background: "#191a1c" }}>
 
-          <div style={{ background: "#191a1c", height: "7vh" }}>
-          </div>
-
-
+          <div style={{ background: "#191a1c", height: "7vh" }}></div>
 
           <div style={{ height: "15vh", color: "whitesmoke", justifyContent: "center", display: "flex" }}>
 
@@ -145,50 +149,54 @@ class Main extends Component {
               <img style={{ width: "20%", display: "inline-block" }} src={insulationmask} />
               <div style={{ width: "80%", display: "inline-block", paddingTop: "3%" }}>
                 <h2>Abatement</h2>
-                <p style={{ width: "80%", fontSize: "125%" }}>
+                <p style={{ width: "80%", fontSize: "125%", paddingTop:"10px" }}>
                   Asbestos, lead, and mold abatement and remediation services.
               </p>
               </div>
             </div>
+
             <div style={{ display: "inline-block", width: "25%", height: "100%", background: "#191a1c" }}>
-              <img style={{ width: "20%", display: "inline-block" }} src={insulationmask} />
+              <img style={{ width: "20%", display: "inline-block" }} src={insulation} />
               <div style={{ width: "80%", display: "inline-block", paddingTop: "3%" }}>
                 <h2>Construction</h2>
-                <p style={{ width: "80%", fontSize: "125%" }}>
+                <p style={{ width: "80%", fontSize: "125%", paddingTop:"10px" }}>
                   Asbestos, lead, and mold abatement and remediation services.
               </p>
               </div>
             </div>
+
             <div style={{ display: "inline-block", width: "25%", height: "100%", background: "#191a1c" }}>
-              <img style={{ width: "20%", display: "inline-block" }} src={insulationmask} />
+              <img style={{ width: "20%", display: "inline-block" }} src={consulting} />
               <div style={{ width: "80%", display: "inline-block", paddingTop: "3%" }}>
                 <h2>Insulation</h2>
-                <p style={{ width: "80%", fontSize: "125%" }}>
+                <p style={{ width: "80%", fontSize: "125%", paddingTop:"10px" }}>
                   Asbestos, lead, and mold abatement and remediation services.
               </p>
               </div>
             </div>
-
+            
           </div>
 
+
+
+          <div style={{ background: "#191a1c", height: "2vh" }}></div>
           {/* -----------------------------------------------------------------VVV  2ND ROW  VVV------------------------------------------------------------------------------------- */}
 
           <div style={{ height: "15vh", color: "whitesmoke", justifyContent: "center", display: "flex" }}>
-
             <div style={{ display: "inline-block", width: "25%", height: "100%", background: "#191a1c" }}>
-              <img style={{ width: "20%", display: "inline-block" }} src={insulationmask} />
+              <img style={{ width: "20%", display: "inline-block" }} src={safety} />
               <div style={{ width: "80%", display: "inline-block", paddingTop: "3%" }}>
                 <h2>Demolition</h2>
-                <p style={{ width: "80%", fontSize: "125%" }}>
+                <p style={{ width: "80%", fontSize: "125%", paddingTop:"10px" }}>
                   Asbestos, lead, and mold abatement and remediation services.
               </p>
               </div>
             </div>
             <div style={{ display: "inline-block", width: "25%", height: "100%", background: "#191a1c" }}>
-              <img style={{ width: "20%", display: "inline-block" }} src={insulationmask} />
+              <img style={{ width: "20%", display: "inline-block" }} src={truck} />
               <div style={{ width: "80%", display: "inline-block", paddingTop: "3%" }}>
                 <h2>Carting</h2>
-                <p style={{ width: "80%", fontSize: "125%" }}>
+                <p style={{ width: "80%", fontSize: "125%", paddingTop:"10px" }}>
                   Asbestos, lead, and mold abatement and remediation services.
               </p>
               </div>
@@ -197,23 +205,19 @@ class Main extends Component {
               <img style={{ width: "20%", display: "inline-block" }} src={insulationmask} />
               <div style={{ width: "80%", display: "inline-block", paddingTop: "3%" }}>
                 <h2>Consulting</h2>
-                <p style={{ width: "80%", fontSize: "125%" }}>
+                <p style={{ width: "80%", fontSize: "125%", paddingTop:"10px" }}>
                   Asbestos, lead, and mold abatement and remediation services.
               </p>
               </div>
             </div>
-
           </div>
 
+          <div style={{ background: "#191a1c", height: "7vh" }}></div>
 
-
-          <div style={{ background: "#191a1c", height: "7vh" }}>
-          </div>
-          
         </div>
 
 
-          {/* ------------------------------------------------------------------------------------------------------------------------------------------------------ */}
+        {/* ------------------------------------------------------------------------------------------------------------------------------------------------------ */}
 
 
 
